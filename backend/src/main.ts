ce8d26fe28a79c3777f613a232e5c72d6be7d14d
SerjRoman <sergeyroman26@gmail.com>
2026-06-03 00:21:54 +0300

    fix: correct path in start:prod script and update CORS origin in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,32 +5,32 @@ import { ValidationPipe } from "@nestjs/common";
 import { Logger } from "nestjs-pino";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, {
-		bufferLogs: true,
-		cors: { origin: "*" },
-	});
-	app.useGlobalPipes(
-		new ValidationPipe({
-			transform: true,
-			whitelist: true,
-		}),
-	);
-	app.useLogger(app.get(Logger));
+    const app = await NestFactory.create(AppModule, {
+        bufferLogs: true,
+        cors: { origin: "https://alias-app-frontend-qzcx.vercel.app/" },
+    });
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+            whitelist: true,
+        }),
+    );
+    app.useLogger(app.get(Logger));
 
-	const config = new DocumentBuilder()
-		.setTitle("API Docs")
-		.setDescription("API Docs description")
-		.setVersion("1.0")
-		.addBearerAuth()
-		.build();
-	const documentFactory = () => SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup("api-docs", app, documentFactory, {
-		swaggerOptions: {
-			persistAuthorization: true,
-		},
-		raw: ["yaml"],
-	});
+    const config = new DocumentBuilder()
+        .setTitle("API Docs")
+        .setDescription("API Docs description")
+        .setVersion("1.0")
+        .addBearerAuth()
+        .build();
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api-docs", app, documentFactory, {
+        swaggerOptions: {
+            persistAuthorization: true,
+        },
+        raw: ["yaml"],
+    });
 
-	await app.listen(process.env.PORT ?? 3000);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
